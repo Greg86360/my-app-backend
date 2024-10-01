2f85b7f85cbe7620b62ff13918634b242918333a
Greg <melozy@free.fr>
2024-10-01 10:31:39 +0200

    Tâches reliées aux utilisateurs

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,10 @@
 const jwt = require('jsonwebtoken');
 
 const auth = (req, res, next) => {
-  const token = req.header('x-auth-token');
-  console.log(token);
+    const token = req.header('Authorization')?.split(' ')[1]; // Récupérer le token après 'Bearer' 
+   
+    console.log('Token reçu:', token); // Log du token
+
   // Vérifier si un token est fourni
   if (!token) {
     return res.status(401).json({ message: 'Accès refusé, token manquant' });
