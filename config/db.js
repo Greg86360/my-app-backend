ae66d08daec7a5d3cef8d7c116052b6f230e9980
Greg <melozy@free.fr>
2024-10-02 10:11:22 +0200

    mise à jour  base de données

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/taches', {
+    await mongoose.connect('mongodb://todolist.9zfsq.mongodb.net/taches', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
